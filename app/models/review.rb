7518fcc8b543e1b0a58c6b16b34f2a870600f3f2
Christian Pinto <pintochristian2@gmail.com>
2019-05-21 11:32:16 -0400

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,14 @@
 class Review
-  
-end
+  attr_accessor :rating, :content
+  attr_reader :restaurant, :customer
+  @@all = []
+  def initialize(restaurant, customer, rating, content)
+    @restaurant, @customer, @rating, @content = restaurant, customer, rating, content
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+end
