4808fc5fa23c2cc9d780961d6428465d74789584
github username <github email address>
2019-05-21 11:31:02 -0400

    tried to complete coding challenge

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,52 @@
 class Restaurant
-  attr_reader :name
+  attr_reader :name, :customers
 
-  def initialize(name)
+
+  @@all = []
+  def initialize(name, customers, reviews)
     @name = name
+    @customers = customers
+    # @average_star_rating = average_star_rating
+    # @longest_review = longest_review
+    @@all << self
   end
 
-end
+  def self.all
+    @@all
+  end
+
+#i want the resturant the customer reviewed to equal self (the restaurant)
+  def customers
+    Restaurant.all.select do |customers|
+      customers.name == self
+    end.uniq
+  end
+
+  #i want the name of the resturant reviewed to equal self (the restaurant)
+  def reviews
+    Review.all.each do |reviews|
+      reviews.name == self
+    end
+    reviews
+  end
+
+
+  def average_star_rating
+
+
+  end
+
+#trying to select the review with the longest length by -----
+  def longest_review
+    Review.all.select do |reviews|
+
+
+
+
+  end
+
+
+
+
+
+end #end of Resturant class
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,21 @@
 class Review
-  
-end
+attr_reader :customer, :resturant
+attr_accessor :rating, :content
+  @@all = []
 
+  def initialize(customer, resturant, content, rating)
+    @customer = customer
+    @resturant = resturant
+    @rating = rating.to_i(1..5)
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
+
+end #end of Review class
