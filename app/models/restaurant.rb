21001a910d5e354af3ddcd7d171e91cb0d27165a
Mendel Stillerman <menstiler@gmail.com>
2019-05-21 11:22:16 -0400

    Done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,38 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  # HELPER METHOD
+  def reviews
+    Review.all.select {|review| review.restaurant == self}
+  end
+
+  def customers
+    reviews.map {|review| review.customer}
+  end
+
+  def average_star_rating
+    sum_rating = reviews.inject(0) {|sum, review| sum += review.rating}
+    sum_rating / reviews.count
+  end
+
+  def longest_review
+    reviews.max_by {|review| review.content.length}
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|restaurant| restaurant.name == name}
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,27 @@
 class Review
-  
-end
 
+  attr_reader :restaurant, :customer, :content, :rating
+
+  @@all = []
+
+  def initialize(restaurant, customer, content, rating)
+    @restaurant = restaurant
+    @customer = customer
+    @content = content
+    
+    if rating.between?(1,5)
+      @rating = rating
+    elsif rating <= 0
+      @rating = 0
+    else
+      @rating = 5
+    end
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
